2e03c4b3dea0e3094d0c4ef92ea2f89770923606
Babatunde <babatundea15@gmail.com>
2020-01-16 10:47:14 +0100

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es6: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+  }
+}
